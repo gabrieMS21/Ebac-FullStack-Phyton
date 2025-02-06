df57c0f58f28c895d3c7998a284194344a45206e
SamurayDev <gabriel.m.s2100@gmail.com>
2025-02-06 11:27:14 -0300

    Add-coments-Fix-messages
    Adicionado comentários para melhor compreensão do código a princípios de aprendizado.
    Arrumado o conflito de mensagens de Correto e Errado.

--- script.js
+++ script.js
@@ -6,28 +6,36 @@ function comparacao(valorA, valorB) {
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
-
-  const campoB = document.querySelector("#field-b");
-  const campoA = document.querySelector("#field-a");
+  //Criado Constantes para caputurar o input dentro do Html
+  const campoB = document.querySelector("#field-b"); //"Document.QuerySelector("#id")"seleciona dentro do Html Que é o "DOCUMENT"
+  const campoA = document.querySelector("#field-a"); //Atráves do comando "QuerySelector" seleciona o ID do campo
 
   //Criado variavel que armazena o valor dos inputs dos campos como NUMBER para não causar conflito
-  const valorA = Number(campoA.value);
+  const valorA = Number(campoA.value); //adicinando a uma variavel constante que captura um valor que se encontra dentro do campo através  do ".Value"
   const valorB = Number(campoB.value);
 
-  const msgError = `Errado! </br> O Campo A: ${campoA.value} é maior que o Campo B: ${campoB.value}`;
-  const msgPositive = ` Correto! </br> O Campo B: ${campoB.value} é maior que o Campo A: ${campoA.value}`;
+  //definindo as mensagens personalizadas de cada um dos possiveis resultados.
+  //E guardando dentro de uma Constante.
+  const msgError = `Errado! </br> O Campo A: ${valorA} é maior que o Campo B: ${valorB}`;
+  const msgPositive = ` Correto! </br> O Campo B: ${valorB} é maior que o Campo A: ${valorA}`;
 
+  //Definindo em uma variavel constante uma seleção dos iD'S de erro e acerto, criados no HTML e escondidos no CSS através do comando "Display: none;".
+  const mensagemContainerError = document.querySelector(".msg-error");
+  const mensagemContainerPositive = document.querySelector(".msg-success");
+
+  //Pegando a variavel constendo os iD'S e definindo para none, Para que toda vez que alguém acione o evento do envio de formulário, o display dessas duas mensagens fiquem escondidas com o NONE no display.
+  mensagemContainerError.style.display = "none";
+  mensagemContainerPositive.style.display = "none";
+
+  //ultilizando a comparação IF com uma função que recebe os dois valores dos campos A e B para acionar determinadas ações.
+  //O ! antes da função de comparação indica que se a função retornar Falso, então o IF irá acionar a primeira ação, se retornar verdadeiro Ele irá acionar o Else que vai mandar a mensagem possitiva.
   if (!comparacao(valorA, valorB)) {
-    const bordaErro = document.querySelector("#field-b");
-    const mensagemContainerError = document.querySelector(".msg-error");
     mensagemContainerError.innerHTML = msgError;
     mensagemContainerError.style.display = "block";
-    bordaErro.style.border = "solid red";
+    campoB.style.border = "solid red";
   } else {
-    const mensagemContainerPositive = document.querySelector(".msg-success");
-    const bordaAcerto = document.querySelector("#field-b");
     mensagemContainerPositive.innerHTML = msgPositive;
     mensagemContainerPositive.style.display = "block";
-    bordaAcerto.style.border = "none";
+    campoB.style.border = "none";
   }
 });

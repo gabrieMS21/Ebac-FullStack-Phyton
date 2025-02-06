144173eb30b4743a4f2351c9d5bc111061f279a8
SamurayDev <gabriel.m.s2100@gmail.com>
2025-02-06 11:49:26 -0300

    add-Coments
    Adicionado Comentários finais ao código, para melhor entendimento acadêmico.

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-const form = document.getElementById("formulario");
+const form = document.getElementById("formulario"); // Primeiro inserimos uma constante com nome form para pegar nosso formulario inteiro e adicionar em uma variavel para que seja mais facil de manipular.
 
+//função de comparação, que compara dois valores de entrada, e retorna como verdadeiro ou falso baseado na comparação
 function comparacao(valorA, valorB) {
-  return valorB > valorA;
+  return valorB > valorA; //retorna verdadeiro se B for maior que A e falso se B não for maior que A.
 }
 
 form.addEventListener("submit", function (e) {
-  e.preventDefault();
+  //Pegando a variável do formulario e adicionando um "Ouvinte" para que toda vez que tiver uma ação dentro do formulário do tipo "Submit" que é acionada pelo nosso botão então ele aciona a função criada abaixo
+  
+  e.preventDefault(); //Funcção que cancela envio do formulário.
+
   //Criado Constantes para caputurar o input dentro do Html
   const campoB = document.querySelector("#field-b"); //"Document.QuerySelector("#id")"seleciona dentro do Html Que é o "DOCUMENT"
   const campoA = document.querySelector("#field-a"); //Atráves do comando "QuerySelector" seleciona o ID do campo
@@ -30,12 +34,20 @@ form.addEventListener("submit", function (e) {
   //ultilizando a comparação IF com uma função que recebe os dois valores dos campos A e B para acionar determinadas ações.
   //O ! antes da função de comparação indica que se a função retornar Falso, então o IF irá acionar a primeira ação, se retornar verdadeiro Ele irá acionar o Else que vai mandar a mensagem possitiva.
   if (!comparacao(valorA, valorB)) {
-    mensagemContainerError.innerHTML = msgError;
-    mensagemContainerError.style.display = "block";
-    campoB.style.border = "solid red";
+    //Ação de erro.
+
+    mensagemContainerError.innerHTML = msgError; //Aqui ele entra dentro do container de mensagens de erro e adiciona a mensagem personalizada criada la em cima no html que está escondido pelo NONE.
+
+    mensagemContainerError.style.display = "block"; //Em seguida muda o estilo do container para block, retirando o NONE e fazendo o container aparecer com a mensagens de erro.
+
+    campoB.style.border = "solid red"; // E aqui ele deixa a borda vermelha do campo que está errado que no caso é o B para indicar aonde está o erro.
   } else {
-    mensagemContainerPositive.innerHTML = msgPositive;
-    mensagemContainerPositive.style.display = "block";
-    campoB.style.border = "none";
+    //Ação de erro.
+
+    mensagemContainerPositive.innerHTML = msgPositive; //Aqui ele entra dentro do container de mensagens de Acerto e adiciona a mensagem personalizada criada la em cima no html que está escondido pelo NONE.
+
+    mensagemContainerPositive.style.display = "block"; //Em seguida muda o estilo do container para block, retirando o NONE e fazendo o container aparecer com a mensagens de Acerto.
+
+    campoB.style.border = "none"; // e se por acaso a borda esteja vermelha, aqui ele muda para borda padrão para poder tirar a impressão de erro já que está certo.
   }
 });
